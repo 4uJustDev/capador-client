1e4e297a9f2c8c73e6664602164a67d6c125c618
Zarubin Alexander <zarubin_14@list.ru>
2025-07-02 21:39:56 +0300

    feat: add google auth

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,5 +1,40 @@
-import { Box } from '@chakra-ui/react'
+import { useState } from 'react'
+import { Box, Container } from '@chakra-ui/react'
+import { GoogleLogin, useGoogleOneTapLogin } from '@react-oauth/google'
+import { jwtDecode } from 'jwt-decode'
 
 export const HomePage = () => {
-  return <Box>START PAGE</Box>
+  const [userProfile, setUserProfile] = useState(null)
+
+  useGoogleOneTapLogin({
+    onSuccess: (credentialResponse) => {
+      const decoded = jwtDecode(credentialResponse.credential)
+      setUserProfile(decoded)
+    },
+    use_fedcm_for_prompt: true
+  })
+
+  return (
+    <Container flex="1">
+      <Box>START PAGE</Box>
+      {userProfile ? (
+        <Box>
+          <p>Welcome, {userProfile.name}</p>
+          <img src={userProfile.picture} alt="Profile" />
+        </Box>
+      ) : (
+        <GoogleLogin
+          onSuccess={(credentialResponse) => {
+            console.log('Google Login Success:', credentialResponse)
+            const decoded = jwtDecode(credentialResponse.credential)
+            console.log('Decoded JWT:', decoded)
+            setUserProfile(decoded)
+          }}
+          onError={() => {
+            console.log('Google Login Failed')
+          }}
+        />
+      )}
+    </Container>
+  )
 }
